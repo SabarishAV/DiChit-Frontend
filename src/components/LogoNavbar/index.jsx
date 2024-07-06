48a799fb852739984b16ba3cfdce2267de9fdf78
Sabarish <avsabarish@gmail.com>
2024-07-06 17:34:13 +0530

    feat: add logo in navbar

---
+++ src/components/LogoNavbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import LogoPath from "../../assets/images/Logo.svg"
+
+const LogoNavbar = ()=>{
+    return <>
+    <nav className="w-screen ">
+        <div className="w-1/4 h-16 bg-no-repeat ml-10 mt-3" style={{ backgroundImage: `url(${LogoPath})` }}></div>
+    </nav>
+    </>
+}
+
+export default LogoNavbar
---
+++ src/pages/SuscriberDetails/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LogoNavbar from "../../components/LogoNavbar";
+
+const SuscriberDetails = ()=>{
+    return (<>
+    <LogoNavbar/>
+    <div className="w-screen h-full">
+        <div className="w"></div>
+    </div>
+    </>)
+}
+
+export default SuscriberDetails;
